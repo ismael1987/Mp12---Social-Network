4b8c42accccd7660fdb2774831e0d6baa2892903
ismael1987 <asmel-1987@hotmail.com>
2018-04-08 22:50:10 +0200

    first social network

---
+++ src/main/java/com/Mp12SocialNetwork/Mp12/Social/Network/Mp12SocialNetworkApplication.java
@@ -0,0 +1,12 @@
+package com.Mp12SocialNetwork.Mp12.Social.Network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp12SocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp12SocialNetworkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Mp12SocialNetwork/Mp12/Social/Network/Mp12SocialNetworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Mp12SocialNetwork.Mp12.Social.Network;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp12SocialNetworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
